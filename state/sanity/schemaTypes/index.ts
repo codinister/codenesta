9772deeed4c572c55394691eeed8663f2f8a677e
codinister <codinister@gmail.com>
2026-04-17 13:10:42 +0100

    Setup sanity studio

---
+++ state/sanity/schemaTypes/index.ts
@@ -0,0 +1,18 @@
+import blockContent from './blockContent';
+import gallery from './object/gallery';
+import imageCard from './object/imageCard';
+import textCard from './object/textCard';
+import titleCard from './object/titleCard';
+import imageObj from './object/imageObj';
+import about from './document/about';
+
+
+export const schemaTypes = [
+  blockContent,
+  imageObj,
+  gallery,
+  imageCard,
+  textCard,
+  titleCard, 
+  about
+];
