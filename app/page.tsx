124a14138e7760a50147b03d6bb3e81d277c49e7
codinister <codinister@gmail.com>
2026-05-23 15:30:33 +0100

    stat gap fixed

--- app/page.tsx
+++ app/page.tsx
@@ -115,9 +115,9 @@ const Home = () => {
           </motion.div>
         </div>
       </div>
-      <div className="gap-6 mx-auto container flex sm:justify-between   p-8 sm:p-0 flex-wrap">
+      <div className="gap-4 mx-auto container flex sm:justify-between   p-8 sm:p-0 flex-wrap">
         {data[0]?.stat.map((v, k) => (
-          <div key={k} className="basis-35 grow">
+          <div key={k} className="basis-30 grow">
             <StatsBox num={v.total} para1={v.title} para2={v.sub_title} />
           </div>
         ))}
